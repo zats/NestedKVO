8d791f3bd48018405954e2c28f802bf8a0353976
Sasha Zats <sasha@zats.io>
2014-08-01 12:24:38 +0300

    Initial Commit

---
+++ NestedKVO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NestedKVO
+//
+//  Created by Sasha Zats on 8/1/14.
+//  Copyright (c) 2014 Sash Zats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NestedKVO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NestedKVO
+//
+//  Created by Sasha Zats on 8/1/14.
+//  Copyright (c) 2014 Sash Zats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
